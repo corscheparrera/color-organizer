2ba4f421aa62bceaab28af685a9d31b3fd6b4ad2
Maxime Lussier <m.lussier@live.ca>
2018-02-19 17:54:19 -0500

    yarn setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,65 @@
-var webpack = require("webpack")
-var path = require("path")
+var webpack = require('webpack')
+var path = require('path')
 
 process.noDeprecation = true
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.join(__dirname, 'dist', 'assets'),
-        filename: "bundle.js",
-        sourceMapFilename: 'bundle.map'
-    },
-    devtool: '#source-map',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['env', 'stage-0', 'react']
-                }
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist', 'assets'),
+    filename: 'bundle.js',
+    sourceMapFilename: 'bundle.map',
+  },
+  devtool: '#source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'stage-0', 'react'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [require('autoprefixer')],
             },
-            {
-                test: /\.css$/,
-                use: ['style-loader','css-loader', {
-                    loader: 'postcss-loader',
-                    options: {
-                      plugins: () => [require('autoprefixer')]
-                    }}]
+          },
+        ],
+      },
+      {
+        test: /\.scss/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [require('autoprefixer')],
             },
-            {
-                test: /\.scss/,
-                use: ['style-loader','css-loader', {
-                    loader: 'postcss-loader',
-                    options: {
-                      plugins: () => [require('autoprefixer')]
-                    }}, 'sass-loader']
-            }
-        ]
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            "process.env": {
-                NODE_ENV: JSON.stringify("production")
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true,
-            warnings: false,
-            mangle: false
-        })
-    ]
+          },
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      warnings: false,
+      mangle: false,
+    }),
+  ],
 }
